73ef4e310120ef122987e7857307228594de47db
Techjeeban <161047962+Techjeeban@users.noreply.github.com>
2025-02-12 12:39:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "taskbucket0956"
+    key    = "statefile/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
