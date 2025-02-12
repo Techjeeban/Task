de4f47f3fcc6a8a8b12c7077158746f68fbede41
Techjeeban <161047962+Techjeeban@users.noreply.github.com>
2025-02-12 15:37:40 +0530

    Create main.tf

---
+++ modules/route_table/main.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "rt" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.gateway_id
+  }
+}
+
+output "rt_id" {
+  value = aws_route_table.rt.id
+}
