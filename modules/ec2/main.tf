b9aa03e745fa9613153e05eac32e9cc2be9b262b
Techjeeban <161047962+Techjeeban@users.noreply.github.com>
2025-02-12 15:42:21 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.key_name
+  associate_public_ip_address = var.associate_public_ip_address
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
